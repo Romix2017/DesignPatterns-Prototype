9f362856f935346a1c01c6ced71f1b5e576690ea
Roman Karpov <poma.05@mail.ru>
2020-02-03 15:38:10 +0400

    init

---
+++ Prototype/Prototype/Prototype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public abstract class Prototype
+    {
+        private string id;
+        public Prototype(string id)
+        {
+            this.id = id;
+        }
+        public string Id
+        {
+            get { return id; }
+        }
+        public abstract Prototype Clone();
+    }
+}
---
+++ Prototype/PrototypeRealWorld/ColorPrototype.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeRealWorld
+{
+    public abstract class ColorPrototype
+    {
+        public abstract ColorPrototype Clone();
+    }
+}
